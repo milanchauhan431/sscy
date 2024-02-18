6b544bd4dd03b2ff9bf607bf58dc398004a16fea
Milan Chauhan <81899573+milanchauhan431@users.noreply.github.com>
2024-02-18 17:41:41 +0530

    Update form.php

--- application/views/app/item_master/form.php
+++ application/views/app/item_master/form.php
@@ -47,7 +47,7 @@
                     <select  id="category" data-input_id="category_id" data-placeholder="Select Category" class="form-control multiSelectBox multiselect" multiple="multiple">
                         <?php
                             foreach($itemCategoryList as $row):
-                                $selected = (!empty($dataRow->category_id) && in_array($row->id,explod(",",$dataRow->category_id)))?"selected":"";
+                                $selected = (!empty($dataRow->category_id) && in_array($row->id,explode(",",$dataRow->category_id)))?"selected":"";
                                 echo '<option value="'.$row->id.'" '.$selected.'>'.$row->category_name.'</option>';
                             endforeach;
                         ?>
