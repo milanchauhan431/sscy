fc3bd85fd05fabb77b46b67ff18c87fc73778fd2
Milan Chauhan <81899573+milanchauhan431@users.noreply.github.com>
2024-02-25 23:24:27 +0530

    Update product_list.php

--- application/views/app/orders/product_list.php
+++ application/views/app/orders/product_list.php
@@ -339,6 +339,7 @@ function countCartItems(){
 }
 
 function removeCartItem(keyToRemove){
+    $('.ajaxLoader,.ajaxLoaderImg').show();
     //Remove Item From Local Storage
     var cartItemList = localStorage.getItem("cartItems");
     cartItemList = JSON.parse(cartItemList);
@@ -363,6 +364,7 @@ function removeCartItem(keyToRemove){
         var addToCart = $('<span class="btn btn-warning btn-cart" onclick="itemDetail('+item.item_id+');">Add to Cart</span>');
     }
     $('#lazy-load-trans #action-btn-'+item.item_id+item.group_id).html(addToCart);
+    $('.ajaxLoader,.ajaxLoaderImg').hide();
 }
 
 function calculateCartQty(){
