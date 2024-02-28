e8866d7abc75b4dfb9209566dfcf34ed03e924e5
Milan Chauhan <81899573+milanchauhan431@users.noreply.github.com>
2024-02-28 12:27:03 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 //--------------------------------------------------------------------------
 
 // Version
-var VERSION = 1.4;
+var VERSION = 2;
 
 // Cache name
 var CACHE_NAME = 'cache-version-' + VERSION;
