c1b35fd6dc734cd6ebea0046fe478255adac1156
Milan Chauhan <81899573+milanchauhan431@users.noreply.github.com>
2024-03-10 12:24:08 +0530

    Update Notification.php

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -9,7 +9,7 @@ class Notification extends MY_Controller{
 
         $data['notificationMsg'] = "Notification test successfull.";
         $data['notificationTitle'] = "Test Notification";
-        //$data['callBack'] = base_url('notification');
+        $data['callBack'] = base_url('app/dashboard');
         $data['emp_ids'] = [1];
 
         $result = $this->masterModel->notify($data);
